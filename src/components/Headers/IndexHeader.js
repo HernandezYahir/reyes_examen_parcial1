039921b993c55876e34c25a63bfe0110ed238620
HernandezYahir <yahir.yahe@gmail.com>
2024-10-03 17:57:00 -0600

    cambios para el examen de Yahir Hernandez

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -37,10 +37,12 @@ function IndexHeader() {
             <img
               alt="..."
               className="n-logo"
-              src={require("assets/img/now-logo.png")}
+              src={require("assets/img/YahirHernandez.png")}
             ></img>
-            <h1 className="h1-seo">Now UI Kit.</h1>
-            <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
+            <h1 className="h1-seo">UNIVERSIDAD TECNOLÓGICA DE DURANGO</h1>
+            <h3>YAHIR ANTONIO HERNANDEZ ESCAMILLA</h3>
+            <h4>GESTIÓN DEL PROCESO DE SOFTWARE</h4>
+            <h5>PARCIAL I</h5>
           </div>
           <h6 className="category category-absolute">
             Designed by{" "}
